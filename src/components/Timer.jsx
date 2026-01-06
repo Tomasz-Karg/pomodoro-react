ad8e34a0c85749da549a49958937bd0a985483d2
Tomasz Karg <tomaszkarg@gmail.com>
2026-01-06 14:51:11 +0100

    Added some more documentation

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect} from 'react';
 
 export function Timer( {durationMinutes} ){
 
-
+  // Transform our Timerduration from Minutes to MilliSeconds
   const [time, setTime] = useState(parseInt(durationMinutes) * 60 * 1000);
 
+  //useEffect to Render the Page every time, the state of "time" changes (dependency array). And the
+  //"time" state gets set and saved every 1000 ms to one less second of its previous value.  
   useEffect( () => {
 
     if (time < 0) return;
@@ -15,8 +17,6 @@ export function Timer( {durationMinutes} ){
 
     return () => clearTimeout(timeoutId);
   }, [time]);
-
-
 
 
   return(
